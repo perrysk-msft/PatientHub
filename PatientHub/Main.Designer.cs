2c3b780d189815f884154f5806c296e11dac6cb8
Perry Skountrianos - MSFT <peskount@microsoft.com>
2019-01-23 15:53:41 -0800

    Initial commit: Project structure

---
+++ PatientHub/Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PatientHub
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopToolStrip = new System.Windows.Forms.ToolStrip();
+            this.BottomToolStrip = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // TopToolStrip
+            // 
+            this.TopToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.TopToolStrip.Name = "TopToolStrip";
+            this.TopToolStrip.Size = new System.Drawing.Size(1342, 25);
+            this.TopToolStrip.TabIndex = 0;
+            this.TopToolStrip.Text = "toolStrip1";
+            // 
+            // BottomToolStrip
+            // 
+            this.BottomToolStrip.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomToolStrip.Location = new System.Drawing.Point(0, 518);
+            this.BottomToolStrip.Name = "BottomToolStrip";
+            this.BottomToolStrip.Size = new System.Drawing.Size(1342, 25);
+            this.BottomToolStrip.TabIndex = 1;
+            this.BottomToolStrip.Text = "toolStrip1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1342, 543);
+            this.Controls.Add(this.BottomToolStrip);
+            this.Controls.Add(this.TopToolStrip);
+            this.Name = "FrmMain";
+            this.Text = "Patient Hub Demo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip TopToolStrip;
+        private System.Windows.Forms.ToolStrip BottomToolStrip;
+    }
+}
+
---
+++ PatientHub/setup-reset.sql
@@ -0,0 +1,30 @@
+/**
+Script Name: setup-or-reset
+Description:
+Created By:
+Created On:
+**/
+
+-- Populate Sample Data
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(24566,'Laboni Deb','Agrita Abele','Agrita','1957-12-19','(415) 575-1182','agrita@example.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(96714,'Laboni Deb','Hudson Onslow','Hudson','1995-11-09','(415) 859-7182','hudsono@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(45674,'Laboni Deb','Isabella Rupp','Isabella','1983-06-09','(415) 555-0102','isabellar@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(26875,'Laboni Deb','Eva Muirden','Eva','1991-05-23','(415) 555-0102','evam@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(70087,'Antonio Klaus','Sophia Hinton','Sophia','1993-02-03','(415) 555-0102','sophiah@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(45687,'Antonio Klaus','Amy Trefl','Amy','1990-05-20','(415) 555-0102','amyt@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(56788,'Antonio Klaus','Anthony Grosse','Anthony','1961-02-14','(415) 555-0102','anthonyg@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(12329,'Antonio Klaus','Alica Fatnowna','Alica','1993-06-14','(415) 555-0102','alicaf@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(45690,'Antonio Klaus','Stella Rosenhain','Stella','1988-09-29','(415) 555-0102','stellar@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(76841,'Antonio Klaus','Ethan Onslow','Ethan','1997-03-18','(415) 555-0102','ethano@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(90872,'Antonio Klaus','Henry Forlonge','Henry','1950-03-11','(415) 555-0102','henryf@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(27312,'Antonio Klaus','Hudson Hollinworth','Hudson','1972-06-15','(415) 555-0102','hudsonh@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(89076,'Lindsey Allen','Lily Code','Lily','1957-05-17','(425) 785-9102','lilyc@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(90718,'Lindsey Allen','Taj Shand','Taj','1978-10-24','(425) 985-8934','tajs@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(46716,'Lindsey Allen','Archer Lamble','Archer','1969-12-12','(206) 455-0672','archerl@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(00917,'Lindsey Allen','Piper Koch','Piper','1954-11-08','(425) 555-0102','piperk@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(66518,'Lindsey Allen','Katie Darwin','Katie','1960-06-16','(206) 777-0802','katied@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(14967,'Lindsey Allen','Jai Shand','Jai','1981-05-02','(206) 999-0900','jais@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(23630,'Lindsey Allen','Jack Potter','Jack','1955-12-27','(425) 444-0322','jackp@wideworldimporters.com',NULL);
+INSERT INTO dbo.Patient (MRN,  PCP, FullName, PreferredName, DOB, PhoneNumber, EmailAddress, Photo) values(94440,'Lindsey Allen','Josef Benda','Josef','1997-06-28','(425) 000-0300','jackp@wideworldimporters.com',NULL);
+
+
