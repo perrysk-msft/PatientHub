6c90736475914222fb6a7fef4f7ecd6be993ea83
Perry Skountrianos - MSFT <peskount@microsoft.com>
2019-01-24 16:30:41 -0800

    Mockups v1 and minor .net config changes

--- DAL/SQL.cs
+++ DAL/SQL.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
-namespace Data
+namespace PatientHub
 {
-    public class SQL
+    public class DbConnection
     {
+        private static string connectionString = ConfigurationManager.AppSettings["Db"];
+        private static string spGetPatients = ConfigurationManager.AppSettings["spGetPatients"];
+        private static int commandTimeout = int.Parse(ConfigurationManager.AppSettings["commandTimeout"]);
+
+        public static async Task GetPatients()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = commandTimeout;
+                    command.CommandText = spGetPatients;
+
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
     }
 }
--- PatientHub/Main.cs
+++ PatientHub/Main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,10 +13,14 @@ namespace PatientHub
 {
     public partial class Main : Form
     {
+        
+
         public Main()
         {
             InitializeComponent();
+            
         }
+
 
     }
 }
